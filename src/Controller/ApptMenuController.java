7603aaf682954ae09a25c90bd637adcf231f0d01
Joshua <joshuapperry4@gmail.com>
2022-10-20 04:01:57 -0600

    Declare event handlers

--- src/Controller/ApptMenuController.java
+++ src/Controller/ApptMenuController.java
@@ -1,4 +1,30 @@
 package Controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
 public class ApptMenuController {
+    @FXML
+    public void onActionCustomerMenu(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    public void onActionApptMenu(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    public void onActionReportsMenu(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    public void onActionAddAppt(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    public void onActionModifyAppt(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    public void onActionExit(ActionEvent actionEvent) {
+    }
 }
--- src/Controller/ModifyApptController.java
+++ src/Controller/ModifyApptController.java
@@ -1,4 +1,14 @@
 package Controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
 public class ModifyApptController {
+    @FXML
+    public void onActionSaveAppt(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    public void onActionCancel(ActionEvent actionEvent) {
+    }
 }
