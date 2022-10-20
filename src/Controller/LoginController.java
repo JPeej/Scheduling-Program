eec037dea284a95d131e7b1de78ef1a12909b299
Joshua <joshuapperry4@gmail.com>
2022-10-20 02:45:26 -0600

    Downgrade IntelliJ

---
+++ src/Controller/LoginController.java
@@ -0,0 +1,16 @@
+package Controller;
+
+import javafx.fxml.Initializable;
+import javafx.stage.Stage;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class LoginController implements Initializable {
+
+    Stage stage;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+}
--- src/Main/Main.java
+++ src/Main/Main.java
@@ -1,11 +1,25 @@
 package Main;
 
 import DAO.JDBC;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
-public class Main {
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/view/Login.fxml"));
+        primaryStage.setTitle(("Login"));
+        primaryStage.setScene(new Scene(root, 270, 240));
+        primaryStage.show();
+    }
 
     public static void main(String[] args) {
 
+        launch(args);
         JDBC.openConnection();
         JDBC.closeConnection();
 
