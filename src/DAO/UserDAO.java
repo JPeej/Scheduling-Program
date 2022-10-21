07746feadaff9c2020534556d24e25d393387540
Joshua <joshuapperry4@gmail.com>
2022-10-21 02:46:59 -0600

    Update javadoc

--- src/DAO/UserDAO.java
+++ src/DAO/UserDAO.java
@@ -3,8 +3,10 @@ package DAO;
 import Model.User;
 import java.sql.SQLException;
 
+/**Interface extension of DAO for User objects. */
 public interface UserDAO extends DAO {
 
+    /**Overloaded method for user authentication on login menu. */
     User get(String userNameLogin, String passwordLogin) throws SQLException;
 
 }
--- src/DAO/UserDAOImp.java
+++ src/DAO/UserDAOImp.java
@@ -6,8 +6,16 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
 
+/**CRUD class for User objects. */
 public class UserDAOImp implements UserDAO{
 
+    /**Retrieve of CRUD method for User objects.
+     * Overloaded method for user authentication on login menu.
+     * Passes SQL query to client_schedule database.
+     * @param userNameLogin String.
+     * @param passwordLogin String.
+     * @return User if found and null if not.
+     * */
     @Override
     public User get(String userNameLogin, String passwordLogin) throws SQLException {
         String sql = "SELECT User_ID, User_Name, Password FROM users WHERE User_Name = ? AND Password = ?";
@@ -19,8 +27,7 @@ public class UserDAOImp implements UserDAO{
             int userID = rs.getInt("User_ID");
             String userName = rs.getString("User_Name");
             String password = rs.getString("Password");
-            User confirmedUser = new User(userID, userName, password);
-            return confirmedUser;
+            return new User(userID, userName, password);
         } else {
             return null;
         }
