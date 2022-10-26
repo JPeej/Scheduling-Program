36014a1ebd6b8001d3de1983e05740e4eaa2297b
Joshua <joshuapperry4@gmail.com>
2022-10-25 19:14:22 -0600

    Add cust combo box populate

--- src/Controller/AddCustomerController.java
+++ src/Controller/AddCustomerController.java
@@ -1,12 +1,26 @@
 package Controller;
 
+import Utility.Locations;
 import Utility.Nav;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-public class AddCustomerController {
+public class AddCustomerController implements Initializable {
 
+    @FXML private ComboBox<String> countryCombo;
+    @FXML private ComboBox divCombo;
+    @FXML private TextField nameText;
+    @FXML private TextField zipText;
+    @FXML private TextField phoneText;
+    @FXML private TextField addressText;
     Nav nav = new Nav();
 
     /**Event handler to Customer Menu.
@@ -15,7 +29,10 @@ public class AddCustomerController {
      * @param actionEvent ActionEvent instantiated via event handler tied to button.*/
     @FXML
     public void onActionSaveCustomer(ActionEvent actionEvent) throws IOException {
-        nav.toCustomersMenu(actionEvent);
+        String string = nameText.getText();
+        String zip = zipText.getText();
+        String phone = phoneText.getText();
+        String address = addressText.getText();
     }
 
     /**Event handler to Customer Menu.
@@ -25,5 +42,25 @@ public class AddCustomerController {
     @FXML
     public void onActionCancel(ActionEvent actionEvent) throws IOException {
         nav.toCustomersMenu(actionEvent);
+    }
+
+    public void onActionCountryCombo(ActionEvent actionEvent) {
+        String countrySelection = (String)countryCombo.getValue();
+        switch(countrySelection) {
+            case "U.S":
+                divCombo.setItems(Locations.usDivList);
+                break;
+            case "UK":
+                divCombo.setItems(Locations.ukDivList);
+                break;
+            case "Canada":
+                divCombo.setItems(Locations.canadaList);
+                break;
+        }
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+       countryCombo.setItems(Locations.countries);
     }
 }
