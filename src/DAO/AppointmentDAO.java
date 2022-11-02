07cfe20aa963c286225678fdb801a1bd8d63d33a
Joshua <joshuapperry4@gmail.com>
2022-11-02 10:31:24 -0600

    Update AppointmentDAO.java
    -Removed unused import
    -Removed redundant public method declarations

--- src/DAO/AppointmentDAO.java
+++ src/DAO/AppointmentDAO.java
@@ -2,20 +2,19 @@ package DAO;
 
 import javafx.collections.ObservableList;
 import java.sql.Timestamp;
-import java.util.ArrayList;
 import java.util.HashMap;
 
 public interface AppointmentDAO extends DAO {
 
-    public ObservableList<String> getCustomerNames();
+    ObservableList<String> getCustomerNames();
 
-    public ObservableList<String> getContactNames();
+    ObservableList<String> getContactNames();
 
-    public HashMap<Timestamp, Timestamp> getAppointments(int customerID);
+    HashMap<Timestamp, Timestamp> getAppointments(int customerID);
 
-    public int cusNameToID(String customerName);
+    int cusNameToID(String customerName);
 
-    public int conNameToID(String contactName);
+    int conNameToID(String contactName);
 
-    public boolean appointmentExists(int apptID);
+    boolean appointmentExists(int apptID);
 }
