f32b9f8e7d51b223fad07dd917a94b84accf66ea
Joshua <joshuapperry4@gmail.com>
2022-11-04 22:56:17 -0600

    Reports generate

--- src/Controller/ReportMenuController.java
+++ src/Controller/ReportMenuController.java
@@ -10,16 +10,13 @@ import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
-import javafx.scene.control.ToggleGroup;
+import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
-import javax.swing.event.ChangeListener;
 import java.io.IOException;
 import java.net.URL;
 import java.sql.SQLException;
 import java.sql.Timestamp;
+import java.time.LocalDateTime;
 import java.time.Month;
 import java.time.Year;
 import java.util.List;
@@ -29,21 +26,22 @@ import java.util.stream.Collectors;
 /**Controller for ReportMenu. */
 public class ReportMenuController implements Initializable {
 
+    @FXML private RadioButton contactSchedule;
+    @FXML private RadioButton typeReport;
+    @FXML private RadioButton monthReport;
+    @FXML private RadioButton expiredAppoints;
     @FXML private ComboBox<String> comboBox;
-    @FXML private ToggleGroup reportRadio;
     @FXML private TableView<Appointment> reportTableView;
     @FXML private TableColumn<Appointment, Integer> apptID;
     @FXML private TableColumn<Appointment, String> apptTitle;
     @FXML private TableColumn<Appointment, String> apptDescript;
-    @FXML private TableColumn<Appointment, String> apptLoc;
+    @FXML private TableColumn<Appointment, String> custID;
     @FXML private TableColumn<Appointment, String> apptType;
     @FXML private TableColumn<Appointment, String> apptContact;
     @FXML private TableColumn<Appointment, String> apptCustomer;
     @FXML private TableColumn<Appointment, Timestamp> apptStart;
     @FXML private TableColumn<Appointment, Timestamp> apptEnd;
     Nav nav = new Nav();
-    private final String comboValue = null;
-    private final int year = Year.now().getValue();
     private ObservableList<Appointment> reportData = null;
     private ObservableList<Appointment> filteredData = null;
     AppointmentDAO appointmentDAO = new AppointmentDAOImp();
@@ -93,58 +91,74 @@ public class ReportMenuController implements Initializable {
         System.exit(0);
     }
 
-    public void onActionScheduleReport(ActionEvent actionEvent) {
-
-    }
-
-    public void onActionTypeReport(ActionEvent actionEvent) {
-        comboBox.setItems(Appointment.types);
-        comboBox.setPromptText("Select type");
-
-        comboBox.getSelectionModel().selectedItemProperty().addListener(
-                (obs, oldValue, newValue) -> {
-                    String type = newValue;
-                    loadTable(filteredByType(type));
-                }
-        );
-    }
-
-    public ObservableList<Appointment> filteredByType(String type) {
-        List<Appointment> typeData =
-                reportData.stream().filter(appointment ->
-                        appointment.getType().contentEquals(type))
-                        .collect(Collectors.toList());
-        filteredData = FXCollections.observableArrayList(typeData);
-        return  filteredData;
-    }
-
-    public void onActionMonthReport(ActionEvent actionEvent) {
-        comboBox.setItems(months);
-        comboBox.setPromptText("Select month");
-
-        comboBox.getSelectionModel().selectedItemProperty().addListener(
-                (obs, oldValue, newValue) -> {
-                    Month month = Month.valueOf(newValue.toUpperCase());
-                    loadTable(filteredByMonth(month, year));
-                }
-        );
-    }
-
-    public ObservableList<Appointment> filteredByMonth(Month month, int year) {
-        List<Appointment> monthlyData =
-                reportData.stream().filter(appointment ->
-                (appointment.getStartStamp().toLocalDateTime().getMonth().equals(month)
-                && appointment.getStartStamp().toLocalDateTime().toLocalDate().getYear() == year))
-                .collect(Collectors.toList());
-        filteredData = FXCollections.observableArrayList(monthlyData);
-        return filteredData;
-    }
-
-    public void onActionExpired(ActionEvent actionEvent) {
+    public void setCombo(String group)  {
+        try {
+            reportTableView.getItems().clear();
+            switch (group) {
+                case "Contact":
+                    comboBox.setItems(appointmentDAO.getContactNames());
+                    break;
+                case "Month":
+                    comboBox.setItems(months);
+                    break;
+                case "Type":
+                    comboBox.setItems(Appointment.types);
+                    break;
+            }
+            comboBox.setPromptText("Select a: " + group);
+        } catch (SQLException e) {
+            MyAlerts.alertError("Dropdown menu could not load data.");
+        }
     }
 
     public void onActionCombo() {
+        try {
+            String comboValue = comboBox.getValue();
+            if (contactSchedule.isSelected()) filterContactSchedule(comboValue);
+            else if (monthReport.isSelected()) filterCustomerMonth(comboValue);
+            else if (typeReport.isSelected()) filterCustomerType(comboValue);
+        } catch (NullPointerException ignored) {}
     }
+
+    public void filterContactSchedule(String name) {
+        List<Appointment> list =
+                reportData.stream().filter
+                (appointment -> appointment.getContact().contentEquals(name))
+                .collect(Collectors.toList());
+        ObservableList<Appointment> contactSchedule = FXCollections.observableArrayList(list);
+        loadTable(contactSchedule);
+    }
+
+    public void filterCustomerMonth(String month) {
+        List<Appointment> list =
+                reportData.stream().filter
+                        (appointment -> appointment.getStartStamp().toLocalDateTime().getMonth()
+                                .equals(Month.valueOf(month.toUpperCase())))
+                .collect(Collectors.toList());
+        ObservableList<Appointment> monthSchedule = FXCollections.observableArrayList(list);
+        loadTable(monthSchedule);
+    }
+
+    public void filterCustomerType(String type) {
+        List<Appointment> list =
+                reportData.stream().filter
+                        (appointment -> appointment.getType().contentEquals(type))
+                        .collect(Collectors.toList());
+        ObservableList<Appointment> typeSchedule = FXCollections.observableArrayList(list);
+        loadTable(typeSchedule);
+    }
+
+    public void filterExpired() {
+        reportTableView.getItems().clear();
+        comboBox.getItems().clear();
+        List<Appointment> list =
+                reportData.stream().filter
+                        (appointment -> appointment.getStartStamp().toLocalDateTime().isBefore(LocalDateTime.now()))
+                .collect(Collectors.toList());
+        ObservableList<Appointment> expiredSchedule = FXCollections.observableArrayList(list);
+        loadTable(expiredSchedule);
+    }
+
 
     /**Load tableview with Appointment data provided.
      * @param appointments appointments, filtered or not, to populate the tableview*/
@@ -154,7 +168,7 @@ public class ReportMenuController implements Initializable {
         apptTitle.setCellValueFactory(new PropertyValueFactory<>("title"));
         apptDescript.setCellValueFactory(new PropertyValueFactory<>("description"));
         apptType.setCellValueFactory(new PropertyValueFactory<>("type"));
-        apptLoc.setCellValueFactory(new PropertyValueFactory<>("location"));
+        custID.setCellValueFactory(new PropertyValueFactory<>("customerID"));
         apptContact.setCellValueFactory(new PropertyValueFactory<>("contact"));
         apptCustomer.setCellValueFactory(new PropertyValueFactory<>("customer"));
         apptStart.setCellValueFactory(new PropertyValueFactory<>("startStamp"));
@@ -164,12 +178,16 @@ public class ReportMenuController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         try {
-            reportData = appointmentDAO.getAll();
-            loadTable(reportData);
+            loadTable(appointmentDAO.getAll());
+            contactSchedule.setOnAction(e -> setCombo("Contact"));
+            typeReport.setOnAction(e -> setCombo("Type"));
+            monthReport.setOnAction(e -> setCombo("Month"));
+            expiredAppoints.setOnAction(e -> filterExpired());
+            comboBox.setItems(appointmentDAO.getContactNames());
+            comboBox.setPromptText("Select a contact");
         } catch (SQLException e) {
             MyAlerts.alertError("Data retrieval failed.");
         }
+        reportData = FXCollections.observableArrayList(reportTableView.getItems());
     }
-
-
 }
